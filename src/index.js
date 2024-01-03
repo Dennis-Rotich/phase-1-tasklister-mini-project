aaf21a78a6d13edef023986aa423c1d5fc0419a9
Dennis <denkipyator@gmail.com>
2024-01-03 13:49:34 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    createToDo(e.target.newTaskDescription.value)
+    form.reset();
+  })
+  
   // your code here
 });
+//function that creates element for user tasks
+function createToDo(todo){
+  const p = document.createElement('p');
+  p.textContent = `${todo} `;
+  const btn = document.createElement('button');
+  btn.textContent = 'X';
+  btn.addEventListener('click',(e)=>{
+    handleDelete(e);
+  })
+  p.appendChild(btn);
+  const ul = document.querySelector('#tasks');
+  const li = document.createElement('li');
+  ul.appendChild(li);
+  li.appendChild(p);
+}
+// function delete task from list
+function handleDelete(task){
+  task.target.parentNode.parentNode.remove();
+}
